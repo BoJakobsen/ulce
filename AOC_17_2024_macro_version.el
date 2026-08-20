3b13164451169b461cb460a110a47aec05719a07
Bo Jakobsen <boj@boj.dk>
2026-08-20 07:39:42 +0200

    Macro experiments

--- AOC_17_2024_macro_version.el
+++ AOC_17_2024_macro_version.el
@@ -3,140 +3,175 @@
 ;;; Commentary:
 ;; ULCE (Useless Lisp CPU Emulator)
 ;; Lisp version of day 17 from 2024.
-;; Another take on solving this problem, goal is to sketch a more extendable cpu structure.
+;; Another take on solving this problem, goal is to sketch a more extendable VM structure.
+
+;; Very simple (and generally useless) VM from AOC 17 2024.
+;; Memory layout for this VM is integer representation with each block being {op, operand} 
+;; Registers are a,b,c general purpose 
+;; 8 opcodes (0 -> 7) inc jnz (op #3) and print (op #5)
+;; op #99 added to this VM for halt
+
+;; The AOC problem part 1 provides a program/data list, and a initial value for reg a
+;; solution is the output when the program halts (reaches the end of program)
 
 ;;; Code:
 
 (require 'cl-lib)
 
-(defvar ulce-memsize 20
-  "Memory size.")
-(setq ulce-memsize 20) ; set here for applying change on re-eval of buffer
+;; VM setup
+(cl-defstruct ulce-machine
+  "VM structure, r: registers sub-structure, pc: program counter, mem: memory, stop-code."
+  (r-abc (make-ulce-registers)) (pc 0) (mem (make-vector ulce-memsize 0)) (stop-code 99))
 
 (cl-defstruct ulce-registers
   "Register structure."
   (a 0) (b 0) (c 0)
   )
 
-(cl-defstruct ulce-machine
-  "CPU structure, r: registers sub-structure, pc: program counter, mem: memory, stop-code."
-  (r-abc (make-ulce-registers)) (pc 0) (mem (make-vector ulce-memsize 0)) (stop-code 99))
-
-
-
-(defmacro ulce-reg (cpu reg)
-  "Return contents of REG (a,b,c)from CPU."
-  (pcase reg
+(defmacro ulce--decode (vm acc &optional operand)
+  "Register access, return contents of REG (a,b,c) from VM."
+  (pcase acc
     ('a
-     `(ulce-registers-a (ulce-machine-r-abc ,cpu)))
+     `(ulce-registers-a (ulce-machine-r-abc ,vm)))
     ('b
-     `(ulce-registers-b (ulce-machine-r-abc ,cpu)))
+     `(ulce-registers-b (ulce-machine-r-abc ,vm)))
     ('c
-     `(ulce-registers-c (ulce-machine-r-abc ,cpu)))))
+     `(ulce-registers-c (ulce-machine-r-abc ,vm)))
+    ('combo
+     `(ulce--decode-combo-operand ,vm ,operand))
+    ('oper
+     `,operand)))
 
+;; for debug
+;(macroexpand-1 '(ulce--decode vm combo a))
 
-;; (macroexpand-1 '(ulce-reg c ulce-cpu))
-
-
-(defun ulce--define-op (n)
-  "Primitively generate op code table of N entries (to be replaced with macros)."
-  (let ((op (make-vector n nil)))
-    (aset op 0
-          (lambda (cpu operand)
-            (setf (ulce-reg cpu a)  (ash (ulce-reg cpu a) (- (ulce--decode-combo-operand cpu operand))))))
-    (aset op 1
-          (lambda (cpu operand)
-            (setf (ulce-reg cpu b)  (logxor (ulce-reg cpu b)  operand))))
-    (aset op 2
-          (lambda (cpu operand)
-            (setf (ulce-reg cpu b)  (logand (ulce--decode-combo-operand cpu operand) 7 ))))
-    (aset op 3
-          (lambda (cpu operand)
-            (if (not (zerop (ulce-reg cpu a)))  (setf (ulce-machine-pc cpu)  operand)
-              nil)))
-    (aset op 4
-          (lambda (cpu _operand)
-            (setf (ulce-reg cpu b)  (logxor (ulce-reg cpu b)  (ulce-reg cpu c)))))
-    (aset op 5
-          (lambda (cpu operand)
-            (message "%d" (logand (ulce--decode-combo-operand cpu operand) 7))))
-    (aset op 6
-          (lambda (cpu operand)
-            (setf (ulce-reg cpu b)  (ash (ulce-reg cpu a) (- (ulce--decode-combo-operand cpu operand))))))
-    (aset op 7
-          (lambda (cpu operand)
-            (setf (ulce-reg cpu c)  (ash (ulce-reg cpu a) (- (ulce--decode-combo-operand cpu operand))))))
-    op))
-
-(defvar ulce-opcodes nil
-  "Global opcode table.")
-(setq ulce-opcodes (ulce--define-op 8))
-
-(defvar ulce-cpu nil "Default global CPU structure.")
-(setq ulce-cpu (make-ulce-machine)); 
-
-(defun ulce--mem-read (cpu addr)
-  "Retuns contents of memory address ADDR from CPU."
-  (aref (ulce-machine-mem cpu) addr))
-
-(defun ulce--advance (cpu)
-  "Retuns contents from CPU memory address pc, and advance pc to next byte."
- (prog1
-  (ulce--mem-read cpu (ulce-machine-pc cpu))
-  (cl-incf (ulce-machine-pc cpu) 1)))
-
-(defun ulce--decode-combo-operand (cpu operand)
-  "Return current combo-operand for OPERAND using CPU."
+(defun ulce--decode-combo-operand (vm operand)
+  "Return current combo-operand for OPERAND using VM."
   (pcase operand
     (4
-     (ulce-reg cpu a))
+     (ulce--decode vm a))
     (5
-     (ulce-reg cpu b))
+     (ulce--decode vm b))
     (6
-     (ulce-reg cpu c))
+     (ulce--decode vm c))
     (7
      (error "Combo operand 7, is not in use"))
     (operand ; operand 0 -- 3 are passed through
      operand)))
 
-(defun ulce--exec-op (cpu op operand opcodes)
-  "Execute OP with OPERAND on CPU."
-    (funcall (aref opcodes op) cpu operand))
+(defun ulce--mem-read (vm addr)
+  "Retuns contents of memory address ADDR from VM."
+  (aref (ulce-machine-mem vm) addr))
+
+(defmacro ulce--set-op (op-vec op-code out opr1 opr2 expr)
+  "Define OP-CODE in vector OP-VEC."
+  `(aset ,op-vec ,op-code
+         (lambda (vm operand)
+           (let ((out ,out)
+                 (opr1 (ulce--decode vm ,opr1 operand))
+                 (opr2 ,opr2))
+             (setf out ,expr)))))
+
+;; (ulce--set-op op 0 (ulce--decode vm a)
+;;                   a (ulce--decode vm combo operand)
+;;                   (ash opr1 (- opr2)))
+;; (ulce--define-op 8)
+;;(ulce--decode ulce-vm oper 1)
+
+(defun ulce--define-op (n)
+  "Primitively generate op code table of N entries (to be replaced with macros)."
+  (let ((op (make-vector n nil)))
+    (ulce--set-op op 0 (ulce--decode vm a)
+                  a (ulce--decode vm combo operand)
+                  (ash opr1 (- opr2)))
+    ;; (aset op 0
+    ;;       (lambda (vm operand)
+    ;;         (setf (ulce-reg vm a)  (ash (ulce-reg vm a) (- (ulce--decode-combo-operand vm operand))))))
+    (aset op 1
+          (lambda (vm operand)
+            (setf (ulce--decode vm b)  (logxor (ulce--decode vm b)  (ulce--decode vm oper operand)))))
+    (aset op 2
+          (lambda (vm operand)
+            (setf (ulce--decode vm b)  (logand (ulce--decode vm combo operand) 7 ))))
+    (aset op 3
+          (lambda (vm operand)
+            (if (not (zerop (ulce--decode vm a)))  (setf (ulce-machine-pc vm)  operand)
+              nil)))
+    (aset op 4
+          (lambda (vm _operand)
+            (setf (ulce--decode vm b)  (logxor (ulce--decode vm b)  (ulce--decode vm c)))))
+    (aset op 5
+          (lambda (vm operand)
+            (message "%d" (logand (ulce--decode vm combo operand) 7))))
+    (aset op 6
+          (lambda (vm operand)
+            (setf (ulce--decode vm b)  (ash (ulce--decode vm a) (- (ulce--decode vm combo operand))))))
+    (aset op 7
+          (lambda (vm operand)
+            (setf (ulce--decode vm c)  (ash (ulce--decode vm a) (- (ulce--decode vm combo operand))))))
+    op))
+
+
+;; Define
+
+(defun ulce--advance (vm)
+  "Retuns contents from VM memory address pc, and advance pc to next byte."
+ (prog1
+  (ulce--mem-read vm (ulce-machine-pc vm))
+  (cl-incf (ulce-machine-pc vm) 1)))
+
+(defun ulce--exec-op (vm op operand opcodes)
+  "Execute OP with OPERAND on VM."
+    (funcall (aref opcodes op) vm operand))
 ;; some check for bad op would be good
 
-(defun ulce-step (&optional cpu)
-  "Step program on machine CPU. Return nil if program end reached."
-  (let* ((cpu (or cpu ulce-cpu))
+(defun ulce-step (&optional vm)
+  "Step program on machine VM. Return nil if program end reached."
+  (let* ((vm (or vm ulce-vm))
          (opcodes ulce-opcodes)
-         (op (ulce--advance cpu))
-         (operand (ulce--advance cpu))) ; operand is second byte
-         (unless (= op (ulce-machine-stop-code cpu))
-           (ulce--exec-op cpu op operand opcodes)
+         (op (ulce--advance vm))
+         (operand (ulce--advance vm))) ; operand is second byte
+         (unless (= op (ulce-machine-stop-code vm))
+           (ulce--exec-op vm op operand opcodes)
            t)))
 
-(defun ulce-run (&optional cpu pc)
-  "Run program on machine CPU until end of program, optional from PC."
-  (let ((cpu (or cpu ulce-cpu)))
-    (when pc (setf (ulce-machine-pc cpu) pc))
-    (while (ulce-step cpu))))
+(defun ulce-run (&optional vm pc)
+  "Run program on machine VM until end of program, optional start at PC."
+  (let ((vm (or vm ulce-vm)))
+    (when pc (setf (ulce-machine-pc vm) pc))
+    (while (ulce-step vm))))
 
-(cl-defun ulce-load-program  (program &key (cpu ulce-cpu) (a 0) (b 0) (c 0))
-  "Load PROGRAM into mem of CPU, set A B C registers, add stop code."
+;; IO and loader functions
+(cl-defun ulce-load-program  (program &key (vm ulce-vm) (a 0) (b 0) (c 0))
+  "Load PROGRAM into mem of VM, set A B C registers, add stop code."
   (if (> (+ 2 (length program)) ulce-memsize) ; space for stop code needed
       (error "Program is to long")
-    (setf (ulce-reg cpu a) a)
-    (setf (ulce-reg cpu b) b)
-    (setf (ulce-reg cpu c) c)
-    (cl-replace (ulce-machine-mem cpu) (vconcat program (list (ulce-machine-stop-code cpu))))))
+    (setf (ulce-reg vm a) a)
+    (setf (ulce-reg vm b) b)
+    (setf (ulce-reg vm c) c)
+    (cl-replace (ulce-machine-mem vm) (vconcat program (list (ulce-machine-stop-code vm))))))
 
-(defun testit-AOC17 (&optional cpu)
-  "Load and run a test program on CPU (default is the global `ulce-cpu')."
-  (let ((cpu (or cpu ulce-cpu)))
-    (ulce-load-program [2 4 1 3 7 5 4 1 1 3 0 3 5 5 3 0] :cpu cpu :a 37283687 :b 0 :c 0)
-    (ulce-run cpu 0)))
+;; Define global VM and Op-code tabel
 
-;; run test on the global default ulce-cpu
+(defvar ulce-memsize 20
+  "Memory size.")
+(setq ulce-memsize 20) ; set here for applying change on re-eval of buffer
+
+(defvar ulce-opcodes nil
+  "Global opcode table.")
+(setq ulce-opcodes (ulce--define-op 8))
+
+(defvar ulce-vm nil "Default global default VM.")
+(setq ulce-vm (make-ulce-machine)); 
+
+(defun testit-AOC17 (&optional vm)
+  "Load and run a test program on VM (default is the global `ulce-vm')."
+  (let ((vm (or vm ulce-vm)))
+    (ulce-load-program [2 4 1 3 7 5 4 1 1 3 0 3 5 5 3 0] :vm vm :a 37283687 :b 0 :c 0)
+    (ulce-run vm 0)))
+
+;; run test on the global default ulce-vm
 (testit-AOC17)
-;(pp ulce-cpu)
+;(pp ulce-vm)
 
 ;;; AOC_17_2024.el ends here
